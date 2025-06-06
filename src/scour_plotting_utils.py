31622a636614e0853f40a53ea21bdf972cde6e47
crossa-ayres <crossa@ayresassociates.com>
2025-06-06 13:11:01 -0600

    total

--- src/scour_plotting_utils.py
+++ src/scour_plotting_utils.py
@@ -367,9 +367,9 @@ def generate_summary_figure(pier_data_dict,
                 scour_data_copy.append(scour_data_array)
             
                 if i == 1 and iteration == 1:
-                    ax.plot([x[0] for x in scour_data_array], [x[1] for x in scour_data_array], color='red',linestyle=':',linewidth=2, label = "Local Scour (LS) at Pier")
+                    ax.plot([x[0] for x in scour_data_array], [x[1] for x in scour_data_array], color='red',linestyle=':',linewidth=2, label = "Total Scour - 500YR")
                 else:
-                    ax.plot([x[0] for x in scour_data_array], [x[1] for x in scour_data_array], color='red',linestyle=':',linewidth=2)
+                    ax.plot([x[0] for x in scour_data_array], [x[1] for x in scour_data_array], color='grey',linewidth=2, label = "Total Scour - 100YR")
             i+=1
         for station in scour_data_copy:
             
@@ -397,22 +397,22 @@ def generate_summary_figure(pier_data_dict,
             ax.plot(bridge_high_chord['Bent CL Sta'], bridge_high_chord['High Chord Elev'], color='black')
             
             if lateral_stability['Laterally Stable Channel?'].values[0] == 'No':
-                ax.plot([x[0] for x in cl_lsd], [x[1] for x in cl_lsd], color='#E98300', label='Total Scour (LTD + CS + LS)')
+                ax.plot([x[0] for x in cl_lsd], [x[1] for x in cl_lsd], color='#E98300')
             else:
-                ax.plot(ground_line['Offset Station'], ground_line['lt_deg'], color='#E98300', label='Total Scour (LTD + CS + LS)')
+                ax.plot(ground_line['Offset Station'], ground_line['lt_deg'],color='grey',linewidth=2)
             #ax.plot(ground_line['Offset Station'], ground_line["abut_scour"], color='#0073CF', label='Abutment Scour (AS)')
             #ax.plot(ground_line['Offset Station'], ground_line["contract_scour"], color='#FCD450', label='Contraction Scour (CS)')
             #ax.plot([x[0] for x in wse], [x[1] for x in wse], color='blue',linewidth=2,linestyle=':', label='WSE')
         else:
-            ax.plot(ground_line['Offset Station'], ground_line['Elev'], color='green')
+            #ax.plot(ground_line['Offset Station'], ground_line['Elev'], color='green')
                         
             if lateral_stability['Laterally Stable Channel?'].values[0] == 'No':
                 ax.plot([x[0] for x in cl_lsd], [x[1] for x in cl_lsd], color='#E98300')
             else:
-                ax.plot(ground_line['Offset Station'], ground_line['lt_deg'], color='#E98300')
-            ax.plot(ground_line['Offset Station'], ground_line["abut_scour"], color='#0073CF')
-            ax.plot(ground_line['Offset Station'], ground_line["contract_scour"], color='#FCD450')
-            ax.plot([x[0] for x in wse], [x[1] for x in wse], color='blue',linewidth=2,linestyle=':')
+                ax.plot(ground_line['Offset Station'], ground_line['lt_deg'], color='red',linestyle=':',linewidth=2)
+                #ax.plot(ground_line['Offset Station'], ground_line["abut_scour"], color='#0073CF')
+                #ax.plot(ground_line['Offset Station'], ground_line["contract_scour"], color='#FCD450')
+                ax.plot([x[0] for x in wse], [x[1] for x in wse], color='blue',linewidth=2,linestyle=':')
 
 
         iteration += 1
