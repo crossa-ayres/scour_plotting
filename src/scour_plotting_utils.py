37e0e36a48deef2bff0429ce3d68b93e97161e73
crossa-ayres <crossa@ayresassociates.com>
2025-06-06 13:21:26 -0600

    changes

--- src/scour_plotting_utils.py
+++ src/scour_plotting_utils.py
@@ -367,9 +367,9 @@ def generate_summary_figure(pier_data_dict,
                 scour_data_copy.append(scour_data_array)
             
                 if i == 1 and iteration == 1:
-                    ax.plot([x[0] for x in scour_data_array], [x[1] for x in scour_data_array], color='red',linestyle=':',linewidth=2)
-                else:
                     ax.plot([x[0] for x in scour_data_array], [x[1] for x in scour_data_array], color='grey',linewidth=2)
+                else:
+                    ax.plot([x[0] for x in scour_data_array], [x[1] for x in scour_data_array], color='red',linestyle=':',linewidth=2)
             i+=1
         for station in scour_data_copy:
             
