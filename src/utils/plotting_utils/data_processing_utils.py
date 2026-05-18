a6232632f3565b2f502d65232dbebce7725dde03
crossa-ayres <crossa@ayresassociates.com>
2026-05-18 16:10:14 -0600

    m

--- src/utils/plotting_utils/data_processing_utils.py
+++ src/utils/plotting_utils/data_processing_utils.py
@@ -207,8 +207,10 @@ def create_Mainfigure(main_dict,
     plt.hlines(y=wse_elev.values[0]-.6,xmin = ground_line['Offset Station'][station_marker]-1, xmax = ground_line['Offset Station'][station_marker]+1, color='black',linewidth=1)
     plt.hlines(y=wse_elev.values[0]-.95,xmin = ground_line['Offset Station'][station_marker]-0.5, xmax = ground_line['Offset Station'][station_marker]+0.5, color='black',linewidth=1)
     
-    st.write([x[0] for x in cse_data], [x[1] for x in cse_data])
-    ax.scatter([float(x[0]) for x in cse_data],[float(x[1]) for x in cse_data], color='black', marker='o', label=f'CSE')
+    try:
+        ax.scatter([float(x[0]) for x in cse_data],[float(x[1]) for x in cse_data], color='black', marker='o', label=f'CSE')
+    except:
+        pass
    
     
  
