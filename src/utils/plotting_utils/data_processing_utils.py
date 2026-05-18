e0233ee94d0ee0f7650abae7e5eb1ebbf88ef322
crossa-ayres <crossa@ayresassociates.com>
2026-05-18 16:03:47 -0600

    m

--- src/utils/plotting_utils/data_processing_utils.py
+++ src/utils/plotting_utils/data_processing_utils.py
@@ -207,7 +207,7 @@ def create_Mainfigure(main_dict,
     plt.hlines(y=wse_elev.values[0]-.6,xmin = ground_line['Offset Station'][station_marker]-1, xmax = ground_line['Offset Station'][station_marker]+1, color='black',linewidth=1)
     plt.hlines(y=wse_elev.values[0]-.95,xmin = ground_line['Offset Station'][station_marker]-0.5, xmax = ground_line['Offset Station'][station_marker]+0.5, color='black',linewidth=1)
     
-    
+    st.write([x[0] for x in cse_data], [x[1] for x in cse_data])
     ax.scatter([x[0] for x in cse_data],[x[1] for x in cse_data], color='black', marker='o', label=f'CSE')
    
     
